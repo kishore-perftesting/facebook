b1b07ee459f51a2d1a6bb80cddbf80eafd0824bb
kishore1085 <kishoreperf2018@gmail.com>
2023-09-07 09:30:08 +0530

    changes were done master branch

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+i am changing this content  to confict  with develpment brach , we modified same  place  below 9: 29 AM IST 
+
 this is in developement barch
 
 this second change at 8 : 50 AM IST or more 
